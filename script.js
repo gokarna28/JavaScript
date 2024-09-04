11b6f80d46295fe19b0616b39e0cbf5ff995bc32
Gokarna Chaudhay <gokarnachy28@gmail.com>
2024-09-04 21:49:01 +0545

    javaScript practice array string

--- script.js
+++ script.js
@@ -271,25 +271,260 @@ function difference(num) {
 }
 console.log(difference(11));
 
-
 //program to return sum if nums are equal then triple of sum
 function sum(n1, n2) {
   if (n1 == n2) {
     return (n1 + n2) * 3;
   } else {
-      return n1 + n2;
+    return n1 + n2;
   }
 }
-console.log(sum(2,3));
+console.log(sum(2, 3));
 
 //check if one of number is 50 or sum of numbers is 50
-function check(x, y){
-  return (x==50 ||y==50|| (x+y==50));
+function check(x, y) {
+  return x == 50 || y == 50 || x + y == 50;
 }
-console.log(check(20,30));
+console.log(check(20, 30));
 
-//Write a JavaScript program to check whether a given integer is within 20 of 100 or 400.  
-function testHundred(x){
-  return (Math.abs(100-x)<=20 || Math.abs(400-x)<=20);
+//Write a JavaScript program to check whether a given integer is within 20 of 100 or 400.
+function testHundred(x) {
+  return Math.abs(100 - x) <= 20 || Math.abs(400 - x) <= 20;
 }
 console.log(testHundred(380));
+
+//Remove a character at the specified position of a given string and return the new string
+function remove_character(str, remove) {
+  var part1 = str.substring(0, remove);
+  var part2 = str.substring(remove + 1, str.length);
+
+  return part1 + part2;
+}
+console.log(remove_character("gokarna", 1));
+
+//Create a new string from a given string changing the position of first and last characters
+
+function replaceChar(str) {
+  if (str.length <= 1) {
+    return str;
+  }
+
+  mid_char = str.substring(1, str.length - 1);
+
+  return str.charAt(str.length - 1) + mid_char + str.charAt(0);
+}
+
+console.log(replaceChar("go"));
+
+//new string with the first letter is added to front and back
+function newStr(str) {
+  if (str.length <= 1) {
+    return str;
+  }
+  mid_str = str.substring(1, str.length - 1);
+
+  return str.charAt(0) + mid_str + str.charAt(0);
+}
+console.log(newStr("mango"));
+
+//multiple of by 3 and 7
+
+function checkMultiple(num) {
+  if (num % 3 == 0 || num % 7 == 0) {
+    return "multiple of 3 and 7";
+  } else {
+    return "not the multiple of 3 and 7";
+  }
+}
+console.log(checkMultiple(27));
+
+//Create a new string from a given string taking the last 3 characters and added at both the front and back
+function backRepace(str) {
+  if (str.length < 3) {
+    return str;
+  }
+  back = str.substring(str.length - 3);
+  return back + str + back;
+}
+console.log(backRepace("radhe"));
+
+//check if the string starts with java
+
+function checkJava(str) {
+  if (str < 4) {
+    return false;
+  }
+
+  front = str.substring(0, 4);
+
+  if (front == "java") {
+    return true;
+  } else {
+    return false;
+  }
+}
+console.log(checkJava("javaScript"));
+console.log(checkJava("script"));
+
+function check(num) {
+  if (num >= 50 && num <= 99) {
+    return true;
+  } else {
+    return false;
+  }
+}
+console.log(check(52));
+console.log(check(100));
+
+function threeCheck(a, b, c) {
+  return (a >= 50 && a <= 99) || (b >= 50 && b <= 99) || (c >= 50 && c <= 99);
+}
+console.log(threeCheck(20, 50, 99));
+console.log(threeCheck(50, 52, 99));
+
+// check if the script is in the 5th position
+// function script(str) {
+//   if (str < 6) {
+//     return str;
+//   }
+//   var newStr = str;
+
+//   if (str.substring(10, 4) == "script") {
+//     newStr = str.substring(0, 4) + str.substring(10, str.length);
+//   }
+//   return newStr;
+// }
+// console.log(script("javascript"));
+
+//closest value to 100
+function closest(x, y) {
+  if (Math.abs(x - 100) < Math.abs(y - 100)) {
+    return x + " is closer to 100";
+  } else {
+    return y + " is closer to 100";
+  }
+}
+console.log(closest(40, 30));
+
+//ramnge check to 40-60 or 70-100
+function Range(x, y) {
+  if (
+    (x >= 40 && x <= 60 && y >= 40 && y <= 60) ||
+    (x >= 70 && x <= 100 && y >= 70 && y <= 100)
+  ) {
+    return "is in range";
+  } else {
+    return "is not in range";
+  }
+}
+console.log(Range(52, 55));
+console.log(Range(66, 87));
+console.log(Range(74, 87));
+
+//check the char in the string
+function check_str(str, char) {
+  let flag = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (str.charAt(i) == char && i >= 1 && i <= 3) {
+      flag = 1;
+      break;
+    }
+  }
+  if (flag == 1) return true;
+  return false;
+}
+console.log(check_str("gokarna", "o"));
+
+// check the three number that has same last digit
+function checkLast(x, y, z) {
+  if (x % 10 == y % 10 && y % 10 == z % 10 && x % 10 == z % 10) {
+    return true;
+  } else {
+    return false;
+  }
+}
+console.log(checkLast(22, 30, 300));
+console.log(checkLast(10, 30, 300));
+
+//program to make the string upper and lower case
+function upper_lower(str) {
+  if (str.length <= 3) {
+    return str.toUpperCase();
+  }
+
+  front = str.substring(0, 3).toLowerCase();
+  back = str.substring(3, str.length).toUpperCase();
+
+  return front + back;
+}
+console.log(upper_lower("gokarna"));
+console.log(upper_lower("gok"));
+
+function checkSum(x, y) {
+  if (x == 15 || y == 15 || x + y == 15 || x - y == 15) {
+    return true;
+  }
+  return false;
+}
+console.log(checkSum(10, 5));
+console.log(checkSum(10, 10));
+
+//multiple of 7
+function checkMul(a, b) {
+  return Math.abs(a) % 7 == 0 || Math.abs(b) % 7 == 0;
+}
+console.log(checkMul(21, 24));
+console.log(checkMul(20, 24));
+
+//reverse string
+function ReverseString(str) {
+  return str.split("").reverse().join("");
+}
+console.log(ReverseString("javaScript"));
+
+//make the first letter uppercase of sting
+function upperCase(str) {
+  str = str.split(" ");
+  for (var i = 0; i < str.length; i++) {
+    str[i] = str[i][0].toUpperCase() + str[i].substring(1);
+  }
+  return str.join(" ");
+}
+console.log(upperCase("This is test code"));
+
+//given integer to time
+function timeInt(num) {
+  var hour = Math.floor(num / 60);
+  var minute = num % 60;
+  return hour + ":" + minute;
+}
+console.log(timeInt(75));
+
+//short the char of word alphabelically
+function Sort(str) {
+  str = str.split("").sort().join("");
+  return str;
+}
+console.log(Sort("world"));
+
+//match the pattern
+function ab_match(str) {
+  return /a...b/.test(str) || /b...a/.test(str);
+}
+console.log(ab_match("gokarnab"));
+console.log(ab_match("gokarnb"));
+
+//count vowels in string
+function countVowel(str) {
+  let vowel = ["a", "e", "i", "o", "u"];
+  let count = 0;
+
+  for (var i = 0; i < str.length; i++) {
+    if (vowel.includes(str[i].toLowerCase())) {
+      count++;
+    }
+  }
+  return count;
+}
+console.log(countVowel("gokarna"));
+console.log(countVowel("what is this man"));
